3458c97c3cf3d1b4f1664eedd5da2bd661a47522
emono <ababababa0222@gmail.com>
2019-06-12 11:26:36 +0900

    fitst commit

---
+++ main.js
@@ -0,0 +1,4 @@
+document.OnLoad = start();
+function start() {
+  window.alert("えぇ――――っ!!!!まだxvideos見てるのかい!!\n\nミ~￣￣￣＼\n/ ＿＿＿＿亅\n  / ＞ ⌒　⌒｜\n｜/　(･) (･)｜\n  (6――○-○-｜\n    ｜　　　つ　｜\n    ｜　　＿＿_) /\n＼　(＿／ /\n／＼＿＿／\n/　 ＼><∧\n  / /　　Ｖ||\n  / _ /　　 ｜||\n⊂ﾆu＼＿_／Lu⊃\n｜　 / /\n｜　/ /\n｜ / /\n  (ﾆフフ\n")
+}
